980e6d02e34468c250a4fb11f4c22df869999cb3
nkatzenberger <penguindude1016@gmail.com>
2024-11-09 17:50:08 -0500

    Working Calculator Function

--- backend/routes/calc.js
+++ backend/routes/calc.js
@@ -2,21 +2,35 @@
 // Note: please do not forget to export the router!
 
 import express from 'express';
-
+//import path from 'path';
+//const __dirname = new URL('.', import.meta.url).pathname;
 const router = express.Router();
 router
   .route('/')
   .get(async (req, res) => {
-    //TODO: check request params to ensure format
-      //num1 should be a float, num2 should be a float, operator should be a string
-        //all of them must be present, implement these checks into all routes
-    //TODO: implement here
-
-
-    //example result
-    const calculated = 4;
-    //temp response
-    return res.status(200).json({result: calculated});
+    /*res.sendFile(path.join(__dirname,'frontend', 'index.html'));
+    return res.status(200);*/ //If we wanted to have website on server to make things easier, we need to figure out how to replace
+    // __dirname with a working function to get file path in JS
+   
+    let equation = req.query;
+    equation  = JSON.stringify(equation);
+    equation = equation.replace("{", "");
+    equation = equation.replace("}", "");
+    equation = equation.slice(7);
+    equation = equation.replaceAll('"', "");
+    equation = equation.replaceAll("^", '**');
+    //TODO: add solver for x variables
+    
+    let calculated = 0;
+      try { 
+      // Evaluate the expression 
+      calculated = eval(equation); // eval converts string to evaluated equation
+      console.log(`Result: ${calculated}`); //delete when frontend works
+      return res.status(200).json({result: calculated});
+  } catch (error) { 
+    return res.status(200).json({result: "Invalid equation. Please enter a valid mathematical expression."})
+  } 
   });
+  
 
 export default router;
