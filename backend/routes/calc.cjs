13030dd100aa19182fe7a0154b3b7c4afc36ee0f
nkatzenberger <penguindude1016@gmail.com>
2024-11-23 21:07:20 -0500

    Sync website launch function to main from aws branch

--- backend/routes/calc.cjs
+++ backend/routes/calc.cjs
@@ -10,40 +10,43 @@ const router = express.Router();
 router
   .route('/')
   .get(async (req, res) => {
-    /*res.sendFile(path.join(__dirname,'frontend', 'index.html'));
-    return res.status(200);*/ //If we wanted to have website on server to make things easier, we need to figure out how to replace
-    // __dirname with a working function to get file path in JS
-   
+    res.sendFile(path.join(__dirname, '../../frontend/index.html'));
+    return res.status(200);
+  });
+  router
+  .route('/calculator')
+  .get(async (req, res) => {
+
     let equation = req.query;
-    equation  = JSON.stringify(equation);
+    equation = JSON.stringify(equation);
     equation = equation.replace("{", "");
     equation = equation.replace("}", "");
     equation = equation.slice(7);
     equation = equation.replaceAll('"', "");
-    
-    
+
+
     let calculated = 0;
-      try { 
+    try {
       // Evaluate the expression 
-      if (equation.includes("=") && equation.includes("x")){
+      if (equation.includes("=") && equation.includes("x")) {
         let splitEquation = equation.split("=");
-        const lhs = algebra.parse(splitEquation[0].trim()); 
+        const lhs = algebra.parse(splitEquation[0].trim());
         const rhs = algebra.parse(splitEquation[1].trim());
         var combinedEquation = new Equation(lhs, rhs);
         var solution = combinedEquation.solveFor("x");
         console.log(`Result: ${solution.toString()}`);
-        return res.status(200).json({result: solution.toString()}); 
+        return res.status(200).json({ result: solution.toString() });
       }
-      else{
+      else {
         equation = equation.replaceAll("^", '**');
         calculated = eval(equation); // eval converts string to evaluated equation
         console.log(`Result: ${calculated}`); //delete when frontend works
-        return res.status(200).json({result: calculated});
+        return res.status(200).json({ result: calculated });
+      }
+    } catch (error) {
+      return res.status(200).json({ result: "Invalid equation. Please enter a valid mathematical expression." })
     }
-  } catch (error) { 
-    return res.status(200).json({result: "Invalid equation. Please enter a valid mathematical expression."})
-  } 
   });
-  
+
 
 module.exports = router;
